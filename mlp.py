2d7963d672acef5d7bfc65a6dd386a9e39c960d7
Jesus Barrera <jesus.ba015@gmail.com>
2017-02-07 15:22:27 -0600

    Add Multilayer Perceptron class

---
+++ mlp.py
@@ -0,0 +1,5 @@
+from neuralnet import MultiLayerPerceptron
+
+if __name__ == '__main__':
+
+    mlp = MultiLayerPerceptron(3, (5, 1))
--- neuralnet.py
+++ neuralnet.py
@@ -38,3 +38,64 @@ class Perceptron:
             return 1
         else:
             return 0
+
+
+class MultiLayerPerceptron:
+    def __init__(self, input_size, shape):
+        self.shape = shape
+        self.output_vectors = []
+        self.weights_array = []
+        self.sensibilities = []
+
+        weights = np.random.random((shape[0], input_size + 1))
+        self.weights_array.append(weights)
+
+        for i in xrange(1, len(shape)):
+            weights = np.random.random((shape[i], shape[i - 1] + 1))
+            self.weights_array.append(weights)
+
+        for i, neurons in enumerate(shape):
+            self.output_vectors.append(np.zeros(neurons + 1))
+
+            self.sensibilities.append()
+
+    def train(
+            self,
+            training_set,
+            learning_rate=0.1
+            min_error=0.1):
+
+        done = False
+
+        while not done:
+            for inputs, output in training_set:
+                self.feed_forward(inputs)
+
+                last = len(self.shape) - 1
+                error = output - self.output_vectors[last]
+
+                self.back_propagation(error)
+
+
+
+    def feed_forward(self, inputs):
+        # first layer
+        for neuron in range(self.shape[0]):
+            activation_value = np.dot(inputs, self.weights_array[0][neuron])
+
+            self.output_vectors[0][neuron] = self.sigmoid(activation_value)
+
+        for layer in xrange(1, len(self.shape)):
+            for neuron in range(self.shape[layer]):
+                layer_inputs = self.output_vectors[layer - 1]
+                activation_value = np.dot(layer_inputs, self.weights_array[layer][neuron])
+
+                self.output_vectors[layer][neuron] = self.sigmoid(activation_value)
+
+    def back_propagation(self, error):
+
+    def sigmoid(self, value):
+        return 1 / (1 + np.exp(-value))
+
+    def diff_sigmoid(self, value):
+        return self.sigmoid(value) * (1 - self.sigmoid(value))
